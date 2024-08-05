bffee7f4cb0926b54cf3fd9087e737712fc687d6
PRO-FLASH101 <jnavaneetharaja@gmail.com>
2024-08-05 12:34:01 +0530

    commit

--- sample_logic.py
+++ sample_logic.py
@@ -1,43 +1,37 @@
 import pandas as pd
 
-#how many employee
-amount = input("How many employees you want to add?\n Enter here: ")
-x = len(amount)
-number = []
+# Number of employees
+amount = int(input("How many employees do you want to add?\nEnter here: "))
+number = list(range(1, amount + 1))
 
-for num in x:
-    number.append(num)
-
-#the times the loop ran
-time = 1
-
-#data
+# Data lists
 name1 = []
 id1 = []
 dob1 = []
 
+# Loop to collect employee data
+for time in number:
+    print(f'Employee {time}')
+    name = input("Enter employee name: ")
+    emp_id = input("Enter employee id: ")
+    dob = input("Enter employee dob(dd/mm/yyyy): ")
+    
+    name1.append(name)
+    id1.append(emp_id)
+    dob1.append(dob)
+
+# Creating the DataFrame
 data = {
     'S no': number, 
     'Name': name1,
     'Emp Id': id1,
     'Dob': dob1,
-    # 'Age': 
 }
+
 df = pd.DataFrame(data)
 print(df)
 
-#loop
-while time <= amount:
-    print(f'Employee {time}')
-    name = input("Enter employee name: ")
-    id = input("Enter employee id: ")
-    dob = input("Enter employee dob(dd/mm/yyyy): ")
-    time += 1 
-    name1.append(name)
-    id1.append(id)
-    dob1.append(dob)
-
-#for debugging
+# For debugging
 print(name1)
 print(id1)
 print(dob1)
