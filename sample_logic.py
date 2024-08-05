5fd7fd6347e4951f8cac5dc0dc19b1c4fbbb8440
PRO-FLASH101 <jnavaneetharaja@gmail.com>
2024-08-05 12:44:42 +0530

    commit

--- sample_logic.py
+++ sample_logic.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from datetime import datetime
 
 # Number of employees
 amount = int(input("How many employees do you want to add?\nEnter here: "))
@@ -8,6 +9,11 @@ number = list(range(1, amount + 1))
 name1 = []
 id1 = []
 dob1 = []
+years = []
+age = []
+for year in years:
+    x = 2024 - year
+    age.append(x)
 
 # Loop to collect employee data
 for time in number:
@@ -15,6 +21,11 @@ for time in number:
     name = input("Enter employee name: ")
     emp_id = input("Enter employee id: ")
     dob = input("Enter employee dob(dd/mm/yyyy): ")
+    
+    # Extract year from dob
+    dob_date = datetime.strptime(dob, "%d/%m/%Y")
+    year = dob_date.year
+    years.append(year)
     
     name1.append(name)
     id1.append(emp_id)
@@ -26,13 +37,9 @@ data = {
     'Name': name1,
     'Emp Id': id1,
     'Dob': dob1,
+    'Age': age
 }
 
 df = pd.DataFrame(data)
 print(df)
 
-# For debugging
-print(name1)
-print(id1)
-print(dob1)
-print(data)
