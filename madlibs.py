cf419a1ceb7c12cb5752b1b261eae22189fa6ade
PRO-FLASH101 <jnavaneetharaja@gmail.com>
2024-08-05 10:22:07 +0530

    commit

--- madlibs.py
+++ madlibs.py
@@ -13,7 +13,7 @@ a = input("") # lacks paprathese update later...
 b = input("")
 c = input("")
 
-# this will be the names
+# this will be the characters
 x = input("") # lacks paprathese update later...
 y = input("")
 z = input("")
@@ -24,7 +24,9 @@ f = input("")
 k = input("")
 
 #The examples
-Example1 = f"Once upon a time, There lived a young {x}. This {x} walked down the street and saw a magical {y}. He wondered what the magical {y} is, then he decided to use the {y}. When he used the {y}, a giant {z} exploded"
+Example1 = f'''Once upon a time, There lived a young {x}. He walked down the street and saw a magical {y}.
+            \n He wondered what the magical {y} is, then he decided to use the {y}. When he used this {y}, a giant {z} exploded.
+            \n From the explosion came out {j} {z}'''
 #example is too long find a way to make it more readable like use new line command
 
 
