1f33e82e7a22e64030c40fa71b23dcda5449268b
PRO-FLASH101 <jnavaneetharaja@gmail.com>
2024-08-05 12:01:18 +0530

    commit

--- madlibs.py
+++ madlibs.py
@@ -21,24 +21,38 @@ z = input("Enter a thing: ")
 #words
 d = input("Enter something unintelligible: ")
 e = input("Enter an angry rebuttal: ")
-f = input("")
+f = input("Enter a word for difficulty: ")
 
 #time
-g = input("Enter an amount of time: ")
-h = input("")
+g = input("Enter an amount of time and include units of time: ")
+h = input("Enter an amount of time and include units of time: ")
 i = input("")
 
 # this will be the colors
 j = input("Enter a color: ")
-f = input("")
+f = input("Enter a color: ")
 k = input("")
 
 #The examples
 Example1 = f'''Once upon a time, There lived a young {x}. He walked down the street and saw a magical {y}. 
             \n He wondered what the magical {y} is, and debated wheter or not to pick it up. Finally, he decided to pick it up.
             \n Then he decided to use the {y}. When he used this {y}, a giant explosion came out of it. From the explosion came out a {j} {z}.
-            \n The {z} said "ARE YOU THE ONE WHO SUMMONED ME!!! YOU DARE AWAKEN ME FROM MY DEEP SLUMBER!" And {x} to frightened to speak stuttered and said "{d}"
-            \n The {z} responded with "What atrocious language are you speaking?" This took {x} about {g} to register, then said "{e}" '''
+            \n The {z} shouted "ARE YOU THE ONE WHO SUMMONED ME!!! YOU DARE AWAKEN ME FROM MY DEEP SLUMBER!" And {x} to frightened to speak stuttered and said "{d}"
+            \n The {z} responded with "What atrocious language are you speaking?" This took {x} about {g} to register, then he screamed "{e}".
+            \n {z} flabbergasted said ""Let's start again. I am {z} the magnificient, and I will grant you one wish if you answer my riddle."
+            \n {x} said I am "{x} the child", {z} then let out a deep chuckle saying "You are no mere child if you awoke me from my slumber, you possess great power" 
+            \n {x} said suprised "what power?" {z} warily said "Let me see, your aura is {f}, this probbaly the rarest auras." {x} shocked about how the {z} 
+            \n found out replied "yea the rarest what good does that do me who can't even do a basic spell". {z} laughing said 
+            \n "Yea you don't do basic spells you do S-class spells, why do you wish to entertain the common people your meant to rule them and be the greatest."
+            \n {x} immeadiet knew what his one wish would be but first he would need to figure out the riddle, so {x} said "What is the riddle". {z} said
+            \n "The riddle is In the realm of mortal coil, I am a vessel of boundless promise, yet my steps echo with unfulfilled destiny. 
+            \n Cloaked in the veils of ignorance, I falter where the fates whisper of my grandeur. 
+            \n Like an unawakened deity, my true essence lies dormant, unnoticed by both gods and men. What am I?"
+            \n {x} was caught off-guard by this riddles {f}, but thought about it and realized what the {z} said eariler. So {x} worried said "The answer is me
+            \n you said I could be great but I didn't realise it so thats why I fail low-level tasks because I lack control of my large power". {z} chuckled
+            \n "hey at least you have a good head on your shoulders, so what is your wish?" {x} suprised said "I was right, letsssss gooooo! My wish is for you to be 
+            \n my teacher and teach me all the things you know". {z} took {h} to respond and said in a deep voice "YOUR WISH IS MY COMMAND. But I will be a tough teacher"
+            \n {x} replied "Okay thats not enough to stop me I will be the greatest, I will prove to everyone I am the best, I will be the WIZARD KING!"'''
 #find a way to change "a" to "an" if a,i,o,u,e
 
 
