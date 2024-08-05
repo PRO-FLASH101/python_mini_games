c8184172a895dfd8727a35aa4f34bed63acd0579
PRO-FLASH101 <jnavaneetharaja@gmail.com>
2024-08-05 14:51:33 +0530

    commit

--- madlibs.py
+++ madlibs.py
@@ -9,12 +9,12 @@ What do I need
 #The inputs for the words
 
 # this will be the adjectives
-# a = input("") # lacks paprathese update later...
+a = input("Enter a magical activity: ") 
 # b = input("")
 # c = input("")
 
 # this will be the characters/tools 
-x = input("Enter a character: ") # lacks paprathese update later...
+x = input("Enter a character: ") 
 y = input("Enter a item: ")
 z = input("Enter a thing: ")
 
@@ -41,7 +41,7 @@ Example1 = f'''Once upon a time, There lived a young {x}. He walked down the str
             \n {x} said I am "{x} the child", {z} then let out a deep chuckle saying "You are no mere child if you awoke me from my slumber, you possess great power" 
             \n {x} said suprised "what power?" {z} warily said "Let me see, your aura is {f}, this probbaly the rarest auras." {x} shocked about how the {z} 
             \n found out replied "yea the rarest what good does that do me who can't even do a basic spell". {z} laughing said 
-            \n "Yea you don't do basic spells you do S-class spells, why do you wish to entertain the common people your meant to rule them and be the greatest."
+            \n "Yea you don't do basic {a} you do S-class {a}, why do you wish to entertain the common people your meant to rule them and be the greatest."
             \n {x} immeadiet knew what his one wish would be but first he would need to figure out the riddle, so {x} said "What is the riddle". {z} said
             \n "The riddle is In the realm of mortal coil, I am a vessel of boundless promise, yet my steps echo with unfulfilled destiny. 
             \n Cloaked in the veils of ignorance, I falter where the fates whisper of my grandeur. 
@@ -52,7 +52,11 @@ Example1 = f'''Once upon a time, There lived a young {x}. He walked down the str
             \n my teacher and teach me all the things you know". {z} took {h} to respond and said in a deep voice "YOUR WISH IS MY COMMAND. But I will be a tough teacher"
             \n {x} replied "Okay thats not enough to stop me I will be the greatest, I will prove to everyone I am the best, I will be the WIZARD KING!"
             '''
-print(Example1)
+
+Example2: f'''
+'''
+
+print(Example1) #delete later..
 #find a way to change "a" to "an" if a,i,o,u,e
 # add a print to actually print out the example!!!
 
