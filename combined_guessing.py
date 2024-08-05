88d2e4045f161f8f1861170846bd322000ea95d3
PRO-FLASH101 <jnavaneetharaja@gmail.com>
2024-08-05 15:19:32 +0530

    commit

---
+++ combined_guessing.py
@@ -0,0 +1,49 @@
+import random
+
+#FOR STAGE 1
+answer_for_easy = 27
+
+# this is the parameter 
+numberoftries = 3
+guesscount = 0
+
+# this is stage 1
+print("")
+print("Hint: the answer is under a 100 but is greater than 10")
+while guesscount < numberoftries:
+    guess = int(input("Guess the number: "))
+    guesscount += 1
+    if guess == answer_for_easy:
+        print("YOU GUESSED THE NUMBER!!!!")
+        print("You passed stage 1 welcome to stage 2")
+        print('')
+        print("STAGE 2: the harder guessing round")
+        print("")
+        # this is how the solution is made
+    partone = random.randrange(1,50)
+    numberguess = int(input("Enter a random number: "))
+    print("Your hint is the number you entered will be multiplied by a random number, so guess the number!!")
+    # this is the solution
+    parttwo = partone * numberguess
+    solution = parttwo
+    # this is the parameter 
+    numberoftries = 3
+    guesscount = 0
+    # this is the game
+    while guesscount < numberoftries:
+        guess = int(input("Guess the number: "))
+        guesscount += 1
+        if guess == solution:
+            print("YOU GUESSED THE NUMBER!!!!")
+            break
+    else:
+        print("")
+        print("The solution was", solution)
+        print("Better luck next time!")
+    # this way the person doesn't get angry at an impossible game 
+    break
+else:
+    print("")
+    print("The solution was", answer_for_easy)
+    print("Better luck next time!")
+    
