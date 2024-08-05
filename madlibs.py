f45f637933bea6ad7154f78efae070127670a709
PRO-FLASH101 <jnavaneetharaja@gmail.com>
2024-08-05 10:54:35 +0530

    commit

--- madlibs.py
+++ madlibs.py
@@ -13,21 +13,33 @@ a = input("") # lacks paprathese update later...
 b = input("")
 c = input("")
 
-# this will be the characters
-x = input("") # lacks paprathese update later...
-y = input("")
-z = input("")
+# this will be the characters/tools 
+x = input("Enter a character: ") # lacks paprathese update later...
+y = input("Enter a item: ")
+z = input("Enter a thing: ")
+
+#words
+d = input("Enter something unintelligible: ")
+e = input("Enter an angry rebuttal: ")
+f = input("")
+
+#time
+g = input("Enter an amount of time: ")
+h = input("")
+i = input("")
 
 # this will be the colors
-j = input("")
+j = input("Enter a color: ")
 f = input("")
 k = input("")
 
 #The examples
-Example1 = f'''Once upon a time, There lived a young {x}. He walked down the street and saw a magical {y}.
-            \n He wondered what the magical {y} is, then he decided to use the {y}. When he used this {y}, a giant {z} exploded.
-            \n From the explosion came out {j} {z}'''
-#example is too long find a way to make it more readable like use new line command
+Example1 = f'''Once upon a time, There lived a young {x}. He walked down the street and saw a magical {y}. 
+            \n He wondered what the magical {y} is, and debated wheter or not to pick it up. Finally, he decided to pick it up.
+            \n Then he decided to use the {y}. When he used this {y}, a giant explosion came out of it. From the explosion came out a {j} {z}.
+            \n The {z} said "ARE YOU THE ONE WHO SUMMONED ME!!! YOU DARE AWAKEN ME FROM MY DEEP SLUMBER!" And {x} to frightened to speak stuttered and said "{d}"
+            \n The {z} responded with "What atrocious language are you speaking?" This took {x} about {g} to register, then said "{e}" '''
+#find a way to change "a" to "an" if a,i,o,u,e
 
 
 
