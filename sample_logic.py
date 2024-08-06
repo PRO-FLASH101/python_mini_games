a048aebd20526ea7f7c9e178f70779cc08d58461
PRO-FLASH101 <jnavaneetharaja@gmail.com>
2024-08-06 12:11:36 +0530

    commit

--- sample_logic.py
+++ sample_logic.py
@@ -4,20 +4,22 @@ from datetime import datetime
 # Number of employees
 while True:
     try:
-        amount = int(input("How many employees do you want to add?\nEnter here: "))
+        employee_amount = int(input("How many employees do you want to add?\nEnter here: "))
         break
     except ValueError:
         print("Enter an integer, no letters or special characters")
 
-print(f'Okay, there are {amount} employees')
-number = list(range(1, amount + 1))
+print(f'Okay, there are {employee_amount} employees')
+number = list(range(1, employee_amount + 1))
 
 # Data lists
-name1 = []
-id1 = []
-dob1 = []
-years = []
-age = []
+employee_list = [
+    [], #name
+    [], #id1
+    [], #dob1
+    [], #years
+    [] #age 
+]
 
 # Loop to collect employee data
 for time in number:
@@ -27,7 +29,7 @@ for time in number:
     while True:
         name = input("Enter employee name: ")
         if name.isalpha():
-            name1.append(name)
+            employee_list[0].append(name)
             break
         else:
             print("Use a name with no numbers or special characters!")
@@ -36,7 +38,7 @@ for time in number:
     while True:
         emp_id = input("Enter employee id: ")
         if emp_id.isdigit():
-            id1.append(emp_id)
+            employee_list[1].append(emp_id)
             break
         else:
             print("Use only numbers, no letters or special characters!")
@@ -46,7 +48,7 @@ for time in number:
         dob = input("Enter employee dob (dd/mm/yyyy): ")
         try:
             dob_date = datetime.strptime(dob, "%d/%m/%Y")
-            dob1.append(dob)
+            employee_list[2].append(dob)
             break
         except ValueError:
             print("Enter in the proper format (dd/mm/yyyy)")
@@ -54,20 +56,20 @@ for time in number:
     # Extract year from dob
     dob_date = datetime.strptime(dob, "%d/%m/%Y") #converts string to int
     year = dob_date.year # extract year 
-    years.append(year) # add year 
+    employee_list[3].append(year) # add year 
     
     # Calc the Age
     current_year = datetime.now().year #get year from current day
     employee_age = current_year - year #gets the current age
-    age.append(employee_age) #adds current age 
+    employee_list[4].append(employee_age) #adds current age 
 
 # Creating the DataFrame
 data = {
-    'S no': number, 
-    'Name': name1,
-    'Emp Id': id1,
-    'Dob': dob1,
-    'Age': age
+    'S no': employee_list[0], 
+    'Name': employee_list[1],
+    'Emp Id': employee_list[2],
+    'Dob': employee_list[3],
+    'Age': employee_list[4]
 }
 
 df = pd.DataFrame(data) #makes the table from the data 
