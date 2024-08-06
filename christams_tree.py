6b97a6b63e61187afb14edd482f4e947bbd2b1e4
PRO-FLASH101 <jnavaneetharaja@gmail.com>
2024-08-06 14:20:53 +0530

    commit

--- christams_tree.py
+++ christams_tree.py
@@ -1,5 +1,11 @@
-'''
-1. draw a base arrow
-2. ask how many triangles
-3. no bugs
-'''
+# to find the amount of triangles
+amount_triangles_wanted = int(input("How many triangles do you want? \nEnter here: "))
+
+#the base stuff
+triangle = "        *    \n      * * *   \n    * * * * *  \n  * * * * * * * \n* * * * * * * * *\n"
+the_arrow = "        *    \n      * * *   \n    * * * * *  \n  * * * * * * * \n* * * * * * * * *\n      * * *   \n      * * *   \n      * * *   \n      * * *   \n      * * *   "
+
+#the christams tree
+final_tree = amount_triangles_wanted * triangle + the_arrow
+
+print(final_tree)
--- playing_triangle.py
+++ playing_triangle.py
@@ -1,6 +1,6 @@
 '''
 1. get input choices 1-6
 2. generate the input using loop
-3. no bugs
+3. no built in 
 '''
 
