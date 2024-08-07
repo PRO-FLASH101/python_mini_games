bbd4e2a0095340a1dc51d33ebba0d0a1668e8083
PRO-FLASH101 <jnavaneetharaja@gmail.com>
2024-08-07 12:17:39 +0530

    commit

--- playing_triangle.py
+++ playing_triangle.py
@@ -1,8 +1,3 @@
-'''
-1. get input choices 1-6
-2. generate the input using loop
-3. no built in 
-'''
 # The info for the triangle
 which_type = int(input("Which type of triangle do you want? (1-6)\n1. Half Pyramid\n2. Inverted Half Pyramid\n3. Hollow Inverted Half Pyramid\n4. Full Pyramid\n5. Inverted Full Pyramid\n6. Hollow Full Pyramid\nEnter Here: "))
 print("")
@@ -39,12 +34,18 @@ def choice3():
             print('* ' + ' ' * (2 * (num - 2)) + '* ')
 
 def choice4():
-    for i in range(height):
-        print("  " * (height - i - 1) + '* ' * (i + 1))
+    for i in range(height, 0, -1):
+        # Leading spaces
+        spaces = '  ' * i
+        # Stars
+        stars = '* ' * (2 * (height - i) - 1)
+        print(spaces + stars)
+    
 
 def choice5():
-    for i in range(height, 0, -1):
-        print('  ' * (height-1) + '* ' * i)
+    for i in range(height):
+        print(' ' * i + '* ' * (height - i))
+
 
 def choice6():
     for i in range(height):
@@ -56,24 +57,26 @@ def choice6():
                 print(' ' * (2 * i - 1) + '*', end='')
             print()
 
-# How to access the option
+# How to access the choices  
 if which_type == 1:
     choice1()
     
 elif which_type == 2:
     choice2()
-
+    
 elif which_type == 3:
     choice3()
-
+        
 elif which_type == 4:
     choice4()
-
+    
 elif which_type == 5:
     choice5()
-
+        
 elif which_type == 6:
     choice6()
+        
 else: 
     print("INVALID CHOICE: Please chose between 1 to 6!")
+        
 
